528ac44bbcdb2a81bdb7e09454137a9c540c90b5
JunkEye <89807197+JunkEye@users.noreply.github.com>
2022-10-27 21:52:42 -0400

    Framework for printer

---
+++ front_end/printer.py
@@ -0,0 +1,27 @@
+import numpy as np
+import gym
+from gym import spaces
+import matplotlib.pyplot as plt
+import random
+import time
+
+class GridEnvironment(gym.Env):
+    
+    def __init__(self):
+        self.observation_space = spaces.Discrete(121)
+    
+    def default(self):
+        #self.agent_pos = [3,1]
+        #self.goal_pos = [0,3]
+        #self.state = np.zeros((5,5))
+        #self.state[tuple(self.agent_pos)] = 1
+        #self.state[tuple(self.goal_pos)] = 0.5
+        observation = self.state.flatten()
+        return observation
+    
+    def print(self, board):
+        NotImplemented
+    
+    def render(self):
+        plt.imshow(self.state)
+        plt.show()
