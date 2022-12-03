e106153a804c6f3328ed579deaf4be73bc0cbbe6
JunkEye <89807197+JunkEye@users.noreply.github.com>
2022-12-02 19:25:47 -0500

    check end of game...?

--- jack.py
+++ jack.py
@@ -20,6 +20,35 @@ def sendMove(lobby, player, roll):
     database.setGame(lobby, game)
     return game["status"]
 
+def checkEnd(lobby):
+    game = database.pullGame(lobby)
+    if game == 0:
+        raise Exception("The end was checked on a game that is not running.")
+    playerList = game["players"]
+    bankruptCount = 0
+    playerUsernames = []
+    for player in playerList:
+        if player["username"] == "INDEBTED INMATE":
+            bankruptCount = bankruptCount + 1
+        else:
+            playerUsernames.append((player["username"], player["money"]))
+    
+    #If there is a winner
+    if len(playerList) - bankruptCount == 1:
+        if len(playerUsernames) != 1:
+            raise Exception("Code error at jack.py")
+        winner = playerUsernames[0]
+
+        database.winGame(winner[0], winner[1])
+        database.endGame(lobby)
+        return True
+    #If something BADLY fucked up
+    elif len(playerList) - bankruptCount == 0:
+        raise Exception("Everyone is bankrupted. This should never ever ever ever happen.")
+    #If the game is still continuing
+    else:
+        return False
+
 def pullBoard(lobby):
     game = database.pullGame(lobby)
     if game == 0:
